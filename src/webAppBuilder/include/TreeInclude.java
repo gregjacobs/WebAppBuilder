6cd7ac4a23200d5c2847cc71c00d9ae173f38290
Gregory Jacobs <gjslick@yahoo.com>
2011-12-27 22:10:36 -0500

    Update doc comments in TreeInclude.java.

--- src/webAppBuilder/include/TreeInclude.java
+++ src/webAppBuilder/include/TreeInclude.java
@@ -10,11 +10,17 @@ import webAppBuilder.*;
  */
 public class TreeInclude extends DirectoryInclude {
 	
+	/**
+	 * Creates a "tree include" directive.
+	 */
 	public TreeInclude( String directory, String fileExtension ) {
 		super( directory, fileExtension );
 	}
 	
 	
+	/**
+	 * Retrieves the collection of files that the TreeInclude represents.
+	 */
 	@Override
 	public Collection<File> getFiles() {
 		return FileHelper.listDirectoryFiles( directory, new ExtensionFilenameFilter(), /* recurse */ true );
