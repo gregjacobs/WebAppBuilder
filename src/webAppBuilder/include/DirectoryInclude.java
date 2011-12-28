7e1413b7a7df085e2eeb62918afa2dbdcead21a4
Gregory Jacobs <gjslick@yahoo.com>
2011-12-27 22:05:39 -0500

    Update doc comments in DirectoryInclude.java.

--- src/webAppBuilder/include/DirectoryInclude.java
+++ src/webAppBuilder/include/DirectoryInclude.java
@@ -13,18 +13,28 @@ public class DirectoryInclude extends Include {
 	protected File directory;
 	protected String fileExtension;
 	
+	
+	/**
+	 * Creates a "directory include" directive.
+	 */
 	public DirectoryInclude( String directory, String fileExtension ) {
 		this.directory = new File( directory );
 		this.fileExtension = fileExtension;
 	}
 	
 	
+	/**
+	 * Retrieves the collection of files that the DirectoryInclude represents.
+	 */
 	@Override
 	public Collection<File> getFiles() {
 		return FileHelper.listDirectoryFiles( directory, new ExtensionFilenameFilter(), /* recurse */ false );
 	}
 	
-	
+	/**
+	 * The FilenameFilter for only accepting files with the fileExtension provided 
+	 * to the DirectoryInclude.
+	 */
 	protected class ExtensionFilenameFilter implements FilenameFilter {
 		@Override
 		public boolean accept( File dir, String name ) { 
