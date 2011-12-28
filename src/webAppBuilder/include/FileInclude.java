8e5255ffa6e74bd04bf4357a76c696d7506f7303
Gregory Jacobs <gjslick@yahoo.com>
2011-12-27 22:07:45 -0500

    Update doc comments in FileInclude.java.

--- src/webAppBuilder/include/FileInclude.java
+++ src/webAppBuilder/include/FileInclude.java
@@ -10,11 +10,23 @@ public class FileInclude extends Include {
 	
 	private File file;
 	
+	
+	/**
+	 * Creates a "file include" directive.
+	 */
 	public FileInclude( String file ) {
 		this.file = new File( file );
 	}
 	
 	
+	/**
+	 * Retrieves the collection of files that the FileInclude represents.
+	 * Because of the nature of a FileInclude, this is only a single file
+	 * (as opposed to other subclasses of Include, which may return more 
+	 * than one).
+	 * 
+	 * @return
+	 */
 	@Override
 	public Collection<File> getFiles() {
 		List<File> list = new LinkedList<File>();
